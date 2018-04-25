cf97ea2b2328ee3dd3f50c1da7f429b2f7832bc9
myerscm <myerscm@kenyon.edu>
2018-04-25 13:32:44 -0400

    Working auth client functionality
    Add sign in, sign up, change password and sign out functionality within the web app

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,96 @@
+'use strict'
+const store = require('../store')
+const signUpSuccess = function () {
+  console.log('sign up is working')
+  $('#authmessage').html('Successfully signed up! Sign in to continue')
+  $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 8000
+  )
+  $('#togglesu').addClass('hidden')
+}
+const signInSuccess = function (data) {
+  $('#authmessage').html('Successfully signed in!')
+  $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+  $('#togglesu').addClass('hidden')
+  $('#togglesi').addClass('hidden')
+  $('#togglecp').removeClass('hidden')
+  $('#toggleso').removeClass('hidden')
+  $('#apibuttons').removeClass('hidden')
+  $('#modalbtn').text('User options')
+  $('form').trigger('reset')
+  store.user = data.user
+}
+
+const signUpFailure = function () {
+  $('#authmessage').html('Failure signing up')
+  $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+}
+const signInFailure = function () {
+  $('#authmessage').html('Failure signing in')
+  $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+}
+const changePasswordSuccess = function () {
+  $('#authmessage').html('Successfully changed password')
+  $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+  $('form').trigger('reset')
+}
+const changePasswordFailure = function () {
+  $('#authmessage').html('Failure changing password')
+  $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+}
+const signOutSuccess = function () {
+  $('#authmessage').html('Successfully signed out')
+  $('#authmessage').css('background-color', 'green')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+  $('#togglesu').removeClass('hidden')
+  $('#togglesi').removeClass('hidden')
+  $('#togglecp').addClass('hidden')
+  $('#toggleso').addClass('hidden')
+  $('#apibuttons').addClass('hidden')
+  $('#usercontent').addClass('hidden')
+  $('#modalbtn').text('Sign in!')
+  store.user = null
+}
+const signOutFailure = function () {
+  $('#authmessage').html('Failure signing out')
+  $('#authmessage').css('background-color', 'red')
+  setTimeout(() => {
+    $('#authmessage').html('')
+  }, 3000
+  )
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInFailure,
+  signInSuccess,
+  changePasswordFailure,
+  changePasswordSuccess,
+  signOutSuccess,
+  signOutFailure
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: '<https://pure-journey-24735.herokuapp.com/>',
   development: 'http://localhost:4741'
 }
 

c5b1828d31122440bae943845de5ce1d1dc9280a
myerscm <myerscm@kenyon.edu>
2018-04-26 15:45:49 -0400

    Fixed production link in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<https://pure-journey-24735.herokuapp.com>',
+  production: 'https://pure-journey-24735.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
